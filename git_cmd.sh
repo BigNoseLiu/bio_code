705b55d6f2a459f70f32bdc38fac69583b832c37
BigNoseLiu <liumingming.2007@163.com>
2022-08-11 16:54:33 +0800

    commit 2022-08-11 16:54:33 by liumm from da_22

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,13 +1,16 @@
 #1.如果新建， 去github 新建一个repository
 
-#2. 创建文件夹
+#2. 配置秘钥
+ssh-keygen -t rsa -C 'liumingming.2007@163.com'
+#将/home/rain/.ssh/id_rsa.pub中的内容拷贝到github的setting/SSH and GPG keys中
+
+#3. 创建文件夹
 git init
 git remote add bio_code git@github.com:BigNoseLiu/bio_code.git
 git pull bio_code master
 
 
-#提交改动
-#3. 提交改动
+#4. 提交改动
 git add .
 git commit -m 'logs'
 git push bio_code master
